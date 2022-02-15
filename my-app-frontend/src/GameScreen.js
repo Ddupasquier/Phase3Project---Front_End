8128456045353f5439f4dc84e8ee67f653968acf
Justin Luciano <justinlluciano@gmail.com>
2022-02-15 14:19:16 -0800

    Started linking the database

--- my-app-frontend/src/GameScreen.js
+++ my-app-frontend/src/GameScreen.js
@@ -2,35 +2,62 @@ import React from "react";
 import Inventory from "./Inventory";
 import Map from "./Map";
 import ScreenText from "./ScreenText";
-
+import { useState, useEffect } from "react";
 
 function GameScreen() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:9292/rooms/9")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <>
       <div className="screentext glitch">
-        <ScreenText />
+        <ScreenText data={data} />
       </div>
       <p>
-  <a class="btn btn-primary map" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Map</a>
-  <button class="btn btn-primary inventory" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Inventory</button>
-  
-</p>
-<div class="row">
-  <div class="col">
-    <div class="collapse multi-collapse" id="multiCollapseExample1">
-      <div class="card cardbg">
-        <Map />
+        <a
+          class="btn btn-primary map"
+          data-toggle="collapse"
+          href="#multiCollapseExample1"
+          role="button"
+          aria-expanded="false"
+          aria-controls="multiCollapseExample1"
+        >
+          Map
+        </a>
+        <button
+          class="btn btn-primary inventory"
+          type="button"
+          data-toggle="collapse"
+          data-target="#multiCollapseExample2"
+          aria-expanded="false"
+          aria-controls="multiCollapseExample2"
+        >
+          Inventory
+        </button>
+      </p>
+      <div class="row">
+        <div class="col">
+          <div class="collapse multi-collapse" id="multiCollapseExample1">
+            <div class="card cardbg">
+              <Map />
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="collapse multi-collapse" id="multiCollapseExample2">
+            <div class="card card-body cardbg">
+              <Inventory />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="collapse multi-collapse" id="multiCollapseExample2">
-      <div class="card card-body cardbg">
-        <Inventory />
-      </div>
-    </div>
-  </div>
-</div>
     </>
   );
 }
--- my-app-frontend/src/ScreenText.js
+++ my-app-frontend/src/ScreenText.js
@@ -1,57 +1,35 @@
 import React from "react";
+import { useState } from "react";
 
-function ScreenText() {
+function ScreenText({ data }) {
+  const [convoId, setConvoId] = useState(1);
+  if (data === null) {
+    return <h2>Loading...</h2>;
+  }
+
+  function loadNextConvoData() {}
+
+  function handleButtonClick(e) {
+    const convo = data.conversations.filter(
+      (c) => c.id === parseInt(e.target.name)
+    );
+    if (convo.length === 0) {
+      loadNextConvoData(parseInt(e.target.name));
+    } else {
+      console.log(convo);
+      setConvoId(convo[0].id);
+    }
+  }
+  const choices = data.conversations[convoId - 1].choices;
+  const buttons = choices.map((c) => (
+    <button onClick={handleButtonClick} name={c.next_conversation_id}>
+      {c.text}
+    </button>
+  ));
   return (
     <>
-    <div className="line">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac rhoncus
-      nisl. Maecenas porttitor, dui sed cursus aliquam, sem ipsum luctus est, in
-      accumsan leo sem posuere massa. Pellentesque at leo elit. Nullam vitae
-      ultricies est, id accumsan justo. Proin consequat ultricies lectus. Fusce
-      massa nulla, finibus ac massa et, lacinia vulputate felis. Morbi aliquet
-      tristique odio, fermentum rhoncus risus congue nec. Morbi lobortis cursus
-      bibendum. Mauris in efficitur turpis, at mattis massa. Nullam ullamcorper
-      pharetra nisi, at scelerisque lectus rhoncus ac. Maecenas aliquet dui
-      egestas ex sagittis, quis suscipit justo ultrices. Suspendisse in libero
-      ut quam malesuada rutrum at ac orci. Phasellus at nibh tortor. Donec
-      aliquam velit a nibh dignissim, sed ultricies ligula gravida. Nam interdum
-      quis nisi eu auctor. Morbi sed tortor sed lectus ullamcorper placerat.
-      Fusce in tristique massa. Nunc at feugiat lacus, eu ultricies sem.
-      Praesent vestibulum rutrum vestibulum. Phasellus id orci tempus, luctus
-      nibh et, facilisis enim. Suspendisse potenti. Donec finibus erat libero,
-      eu lobortis nibh suscipit vitae. Ut malesuada nibh velit. In finibus, est
-      non accumsan auctor, libero mi imperdiet nulla, a condimentum neque magna
-      vel diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-      tristique malesuada est ut rhoncus. Praesent laoreet in ipsum eu
-      venenatis. Sed congue purus orci, ut lacinia diam efficitur ullamcorper.
-      Vestibulum est metus, convallis sit amet lobortis eget, efficitur quis
-      sem. Suspendisse ut nisl eget tellus fermentum luctus. Duis aliquet
-      vestibulum nunc vel lacinia. Phasellus in lorem lectus. Nunc aliquam et
-      urna vitae vehicula. Nullam in odio orci. Nam neque quam, porta at nulla
-      gravida, tincidunt interdum felis. Morbi et imperdiet ipsum. Cras ac
-      sapien ut felis elementum volutpat eget et orci. Quisque dapibus accumsan
-      arcu, non facilisis est consectetur vitae. Curabitur aliquet elit non
-      magna scelerisque, ut commodo odio dignissim. Suspendisse elementum
-      ullamcorper nulla, quis varius nibh placerat in. Sed tempor a nisi eu
-      vulputate. Sed ultrices ligula vitae metus hendrerit, vitae tempus purus
-      luctus. Phasellus porttitor nec urna non tempus. Nam feugiat enim nunc, eu
-      efficitur orci suscipit pulvinar. Pellentesque sed hendrerit nisl.
-      Pellentesque ac orci sit amet nulla posuere viverra. Curabitur turpis
-      nulla, ultricies vitae scelerisque nec, pellentesque in quam. Mauris
-      libero nibh, pretium ac nunc et, ornare ornare neque. Vivamus ullamcorper
-      est a efficitur mattis. Pellentesque faucibus id nisl vel fermentum.
-      Curabitur sapien nisl, efficitur nec semper quis, consectetur ut lectus.
-      Nunc at mi nibh. Vivamus quis augue et odio sodales viverra. Ut et dolor
-      tortor. Integer non commodo urna. Morbi mattis auctor elit, ut porta
-      justo. Duis sagittis urna nisi, vitae suscipit turpis posuere eget. Donec
-      dictum faucibus mi, sed accumsan lorem efficitur quis. Suspendisse
-      volutpat nisi sed elit mollis suscipit. Suspendisse pretium massa nec
-      congue lacinia. Sed feugiat lectus eget maximus dapibus. Morbi non aliquam
-      metus. Integer sit amet vehicula dui. Quisque vulputate sodales est, ac
-      sollicitudin nibh vestibulum sed. Proin tempor rutrum odio at volutpat.
-      Integer consectetur consequat est nec hendrerit. Nam maximus pulvinar
-      nulla ut viverra. Praesent sed pretium ante.
-      </div>
+      <div className="line">{data.conversations[convoId - 1].text}</div>
+      <div>{buttons}</div>
     </>
   );
 }
